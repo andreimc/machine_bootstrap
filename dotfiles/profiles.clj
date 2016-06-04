4b050797edfa462a4fded648af2f8230ff90dbd8
Andrei Miulescu <lusu777@gmail.com>
2016-06-04 19:20:34 +1000

    Update profiles

--- dotfiles/profiles.clj
+++ dotfiles/profiles.clj
@@ -1,3 +1,6 @@
 {:user { :plugins [[lein-cljsbuild "1.1.3"]
                    [cider/cider-nrepl "0.12.0"]
+                   [weasel  "0.7.0"]
+                   [jonase/eastwood  "0.2.3"]
                    [com.cemerick/piggieback  "0.2.1"]] }}
+
