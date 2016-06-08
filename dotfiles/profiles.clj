d2d0e10c206c743a0f1ed823d7a4bf255b6ac283
Andrei Miulescu <lusu777@gmail.com>
2016-06-08 10:36:58 +1000

    Some more stuff for vim rspec

--- dotfiles/profiles.clj
+++ dotfiles/profiles.clj
@@ -2,5 +2,10 @@
                    [cider/cider-nrepl "0.12.0"]
                    [weasel  "0.7.0"]
                    [jonase/eastwood  "0.2.3"]
-                   [com.cemerick/piggieback  "0.2.1"]] }}
+                   [spyscope "0.1.3"]
+                   [redl "0.2.4"]
+                   [com.cemerick/piggieback  "0.2.1"]] 
+        :injections [(require 'spyscope.core)
+                     (require '[redl complete core])]
+        }}
 
