5bbe8885ac35f85145a84685fb492f45f45b1f96
Andrei Miulescu <lusu777@gmail.com>
2016-06-17 21:12:05 +1000

    Some space macs stuff

--- dotfiles/profiles.clj
+++ dotfiles/profiles.clj
@@ -1,7 +1,9 @@
 {:user {:plugins [[lein-cljsbuild "1.1.3"]
                   [lein-clojars "0.9.1"]
+                  [cider/cider-nrepl "0.12.0"]
+                  [cider/cider-nrepl "0.12.0"]
                   [lein-codox  "0.9.5"]
                   [chestnut/lein-template  "0.12.0"]
                   [lein-auto  "0.1.2"]]
-                                }}
-
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
