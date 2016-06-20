dcb28878bdd3f142f03c55fe6218dc2759c75fc3
Andrei Miulescu <lusu777@gmail.com>
2016-06-20 13:45:27 +1000

    Upgrade cider

--- dotfiles/profiles.clj
+++ dotfiles/profiles.clj
@@ -1,7 +1,6 @@
 {:user {:plugins [[lein-cljsbuild "1.1.3"]
                   [lein-clojars "0.9.1"]
-                  [cider/cider-nrepl "0.12.0"]
-                  [cider/cider-nrepl "0.12.0"]
+                  [cider/cider-nrepl "0.13.0-SNAPSHOT"]
                   [lein-codox  "0.9.5"]
                   [chestnut/lein-template  "0.12.0"]
                   [lein-auto  "0.1.2"]]
