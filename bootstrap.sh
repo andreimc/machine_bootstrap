e799ad780b9dfcbf53a867e07b0ec4e48ab411a4
Andrei Miulescu <andrei@Andreis-MBP.local.gateway>
2016-12-20 19:20:19 +1100

    new mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#	* bootstrap machine in order to prepare for ansible playbook run 
+#	* bootstrap machine in order to prepare for ansible playbook run
 set -e
 
 # Download and install Command Line Tools if no developer tools exist
@@ -12,15 +12,35 @@ set -e
 #       * ensure dev tools are installed by checking the output of gcc
 #       * check to see if gcc binary even exists ( original logic )
 # if either of the conditions are met, install dev tools
+
+
 if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
-    echo "Info   | Install   | xcode"
-    xcode-select --install
+  defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
+  defaults write com.apple.dock persistent-apps -array; # remove icons in Dock
+  defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
+  defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
+  defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
+  defaults write com.apple.dock orientation bottom; # place Dock on the right side of screen
+  defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
+  killall Dock 2>/dev/null;
+  killall Finder 2>/dev/null;
+
+# install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" --verbose;
 fi
 
-# Download and install Homebrew
 if [[ ! -x /usr/local/bin/brew ]]; then
-    echo "Info   | Install   | homebrew"
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+sudo ruby \
+  -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+  </dev/null
 fi
 
 # Download and install Ansible
@@ -28,7 +48,7 @@ if [[ ! -x /usr/local/bin/ansible ]]; then
     echo "Info   | Install   | Ansible"
     brew update
     brew install python
-    pip install ansible==1.9.4
+    brew install ansible
 fi
 
 # Download and install Mas for AppStore Automation
@@ -38,7 +58,7 @@ if [[ ! -x /usr/local/bin/mas ]]; then
 fi
 
 # Download and install Java
-if [[ ! -x /usr/bin/java ]]; then
+if ! [[ $(java -version 2>&1) =~ "java version" ]]; then
     echo "Info   | Install   | Java"
     brew tap caskroom/cask
     brew cask install java
